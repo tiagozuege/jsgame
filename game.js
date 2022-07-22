7a5640e93f86be69c95687ebe970b7e3847e2a86
Tiago Zuege <tiago.zuege@gmail.com>
2022-07-22 19:10:16 -0300

    Ajustes na logica e interface para geração de blocos e pontuação

--- game.js
+++ game.js
@@ -1,16 +1,24 @@
-//Creating a simple game with VanillaJS
+//Jogo criado com Vanilla JS
 
 const HEIGHT = 500;
 const WIDTH = 500;
+const BOUNDS = 0;
+const BLOCK_SIZE = 20;
+const REFRESH_INTERVAL = 150;
+const PTS_ELEMENT = document.getElementById('points');
 
 var context;
 var x = 1;
 var y = 20;
 var cont = 0;
 var direction = '';
+var playerHasMoved = false;
+var snakeHasEaten = true;
+var newBlockPositionX = 0;
+var newBlockPositionY = 0;
 
 window.onload = function() {
-
+  PTS_ELEMENT.innerHTML = 0;
   startGame();
 }
 
@@ -20,55 +28,69 @@ function startGame() {
   context.canvas.width = WIDTH;
   context.canvas.height = HEIGHT;
 
-
-
   var canvasUpdate = setInterval(function (x, y, h, w) {
 
     direction = keyPressedHandler();
-    // console.log(direction);
 
-    if (this.x >= HEIGHT || this.x <= 0) {    //atingiu o final do eixo x
+    if (playerHasMoved && snakeHasEaten) {
+      newBlockPositionX = generateRandomBlockPosition();
+      newBlockPositionY = generateRandomBlockPosition();
+      context.fillRect(newBlockPositionX, newBlockPositionY, 15, 15);
+      snakeHasEaten = false;
+    }
+    
+
+    // verifica se nao houve colisao nos limites do canvas
+    if (this.x >= HEIGHT - 9 || this.x <= BOUNDS) { 
       clearInterval(canvasUpdate);
       alert('Game Over!');
     }
-    if (this.y >= WIDTH || this.y <= 0) {
+    if (this.y >= WIDTH - 9 || this.y <= BOUNDS) {
       clearInterval(canvasUpdate);
       alert('Game Over!');
     }
 
-    //context.clearRect( (this.x - 10), this.y, 15, 15);  //limpa o 'x' anterior do canvas
-    // context.fillRect(this.x, this.y, 15, 15);
-
+    
     //directions:
 
     if (direction == 'right') {
+      playerHasMoved = true;
       this.x += 10;
-      context.clearRect( (this.x - 10), this.y, 15, 15);
+      context.clearRect( (this.x - 10), this.y, BLOCK_SIZE, BLOCK_SIZE);
     }
 
     if (direction == 'left') {
+      playerHasMoved = true;
       this.x -= 10;
-      context.clearRect( (this.x + 10), this.y, 15, 15);
+      context.clearRect( (this.x + 10), this.y, BLOCK_SIZE, BLOCK_SIZE);
 
     }
 
     if (direction == 'up') {
+      playerHasMoved = true;
       this.y -= 10;
-      context.clearRect(this.x, (this.y + 10), 15, 15);
+      context.clearRect(this.x, (this.y + 10), BLOCK_SIZE, BLOCK_SIZE);
 
     }
 
     if (direction == 'down') {
+      playerHasMoved = true;
       this.y += 10;
-      context.clearRect(this.x, (this.y - 10), 15, 15);
+      context.clearRect(this.x, (this.y - 10), BLOCK_SIZE, BLOCK_SIZE);
 
     }
 
-    context.fillRect(this.x, this.y, 15, 15);
+    // posicao atualizada apos os movimentos
+    context.fillRect(this.x, this.y, BLOCK_SIZE, BLOCK_SIZE);
 
-  }, 100);   //diminuir para que o canvas renderize mais rápido (dificuldade)
+  }, REFRESH_INTERVAL); 
 
 
+}
+
+var generateRandomBlockPosition = function () {
+  rnd = Math.floor(Math.random() * 490) + 10;
+  return rnd;
 }
 
 function keyPressedHandler() {
@@ -77,13 +99,11 @@ function keyPressedHandler() {
 
     switch (event.keyCode) {
       case 40:
-        // for (var i = 0; i < 1000; i++);
         direction = 'down';
         console.log('Down');
         break;
 
       case 39:
-        // for (var i = 0; i < 1000; i++);
         direction = 'right';
         console.log('Right');
         break;
